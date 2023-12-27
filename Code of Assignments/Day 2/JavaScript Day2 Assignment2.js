d4ee4a958b870a622ebb034df0a1922171408a32
Shivang Saxena <shivangsaxena40@gmail.com>
2023-12-28 01:00:17 +0530

    UPDATE

--- Code of Assignments/Day 2/JavaScript Day2 Assignment2.js
+++ Code of Assignments/Day 2/JavaScript Day2 Assignment2.js
@@ -1,5 +1,3 @@
-// Assignment 2:
-// Write a JavaScript function that takes a positive integer as a parameter and calculates its factorial using a for loop. The factorial of a number N is the product of all positive integers less than or equal to N.
 function factorial(a=5){
     if(a==0 && a==1){
         console.log("Factorial is = 1")
